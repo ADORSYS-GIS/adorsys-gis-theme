2846ebc80256d962c4032a1873527f61be7f2e11
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2025-02-06 11:51:46 +0100

    feat: copy code with button

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import "./css/global.scss";
+import "./libs/code";
 import "./libs/logo";
+import { addFeatherIconToButton } from "./libs/utils";
 
 window.addEventListener("load", () => {
     const languageButton = document.querySelector("#kc-current-locale-link");
@@ -61,17 +63,3 @@ function replaceWrongHr() {
     }
 }
 
-function addFeatherIconToButton(element: Element | null, iconStr: string) {
-    const icon = createIcon(iconStr);
-    if (!element) return;
-
-    element.prepend(icon);
-}
-
-function createIcon(icon: string): HTMLElement {
-    const languageIcon = document.createElement("i");
-    const attr = document.createAttribute("data-feather");
-    attr.value = icon;
-    languageIcon.attributes.setNamedItem(attr);
-    return languageIcon;
-}
